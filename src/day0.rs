49c868badfabd244342886e2cdbddf3e0a0580f5
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2024-12-01 16:57:54 +0100

    day 1 optimize

--- src/day0.rs
+++ src/day0.rs
@@ -1,32 +1,42 @@
-use aoc_runner_derive::{aoc, aoc_generator};
+use ahash::AHashMap;
+use std::str::FromStr;
+
+use aoc_runner_derive::aoc;
 
 pub type Line = ();
+pub type Answer = ();
 
 pub struct Input {
+
 }
 
-impl FromIterator<Line> for Input {
-    fn from_iter<T: IntoIterator<Item = Line>>(iter: T) -> Self {
+impl FromStr for Input {
+    type Err = ();
 
-        Self {
-        }
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parsed_lines = unsafe {
+            s.split_terminator('\n').map(|line| {
+                let mut segments = line
+                    .split_ascii_whitespace()
+                    .map(|seg| seg.parse().unwrap_unchecked());
+                
+            })
+        };
+
+        Ok(Self {
+
+        })
     }
 }
 
-#[aoc_generator(day1)]
-pub fn generator(text: &str) -> Input {
-    Input::from_iter(text.lines().map(|line| {
-        let mut segments = line
-            .split_whitespace()
-            .map(|segment| segment.parse().unwrap());
-        ()
-    }))
-}
-
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &Input) -> u32 {
+pub fn part1(text: &str) -> Answer {
+    let mut input: Input = unsafe { text.parse().unwrap_unchecked() };
+
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &Input) -> u32 {
+pub fn part2(text: &str) -> Answer {
+    let input: Input = unsafe { text.parse().unwrap_unchecked() };
+
 }
--- src/day1.rs
+++ src/day1.rs
@@ -1,43 +1,47 @@
+use ahash::AHashMap;
 use std::str::FromStr;
 
 use aoc_runner_derive::aoc;
 
 pub type Line = (u32, u32);
+pub type Answer = u32;
 
 pub struct Input {
     left_column: Vec<u32>,
     right_column: Vec<u32>,
 }
 
-impl FromIterator<Line> for Input {
-    fn from_iter<T: IntoIterator<Item = Line>>(iter: T) -> Self {
-        let (mut left_column, mut right_column): (Vec<u32>, Vec<u32>) = iter.into_iter().unzip();
-        left_column.sort();
-        right_column.sort();
-
-        Self {
-            left_column,
-            right_column,
-        }
-    }
-}
-
 impl FromStr for Input {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed_lines = s.lines().map(|line| {
-            let mut segments = line.split_whitespace().map(|seg| seg.parse().unwrap());
-            (segments.next().unwrap(), segments.next().unwrap())
-        });
+        let parsed_lines = unsafe {
+            s.split_terminator('\n').map(|line| {
+                let mut segments = line
+                    .split_ascii_whitespace()
+                    .map(|seg| seg.parse::<u32>().unwrap_unchecked());
+                (
+                    segments.next().unwrap_unchecked(),
+                    segments.next().unwrap_unchecked(),
+                )
+            })
+        };
 
-        Ok(Self::from_iter(parsed_lines))
+        let (left_column, right_column) = parsed_lines.unzip();
+
+        Ok(Self {
+            left_column,
+            right_column,
+        })
     }
 }
 
 #[aoc(day1, part1)]
-pub fn part1(text: &str) -> u32 {
-    let input: Input = text.parse().unwrap();
+pub fn part1(text: &str) -> Answer {
+    let mut input: Input = unsafe { text.parse().unwrap_unchecked() };
+
+    radsort::sort(&mut input.left_column);
+    radsort::sort(&mut input.right_column);
 
     input
         .left_column
@@ -47,15 +51,16 @@ pub fn part1(text: &str) -> u32 {
 }
 
 #[aoc(day1, part2)]
-pub fn part2(text: &str) -> u32 {
-    let input: Input = text.parse().unwrap();
+pub fn part2(text: &str) -> Answer {
+    let input: Input = unsafe { text.parse().unwrap_unchecked() };
+
+    let mut appearances = AHashMap::with_capacity(input.right_column.len());
+
+    input.right_column.iter().for_each(|value| {
+        *appearances.entry(value).or_insert(0) += 1;
+    });
 
     input.left_column.iter().fold(0, |acc, left| {
-        acc + input
-            .right_column
-            .iter()
-            .filter(|right| *right == left)
-            .count() as u32
-            * left
+        acc + left * appearances.get(left).unwrap_or(&0)
     })
 }
