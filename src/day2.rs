bbf2ddfdcc1417cff8bd3d6148ac4d4a0e0620ca
Max Pfeil <60489413+ArrowMaxGithub@users.noreply.github.com>
2024-12-03 02:08:30 +0100

    day 2 optimize

--- src/day2.rs
+++ src/day2.rs
@@ -1,30 +1,44 @@
-use std::{collections::BinaryHeap, str::FromStr};
+use std::str::FromStr;
 
 use aoc_runner_derive::aoc;
 
-pub type Line = Vec<i8>;
+#[cfg(test)]
+const N: usize = 6;
+#[cfg(not(test))]
+const N: usize = 1000;
+
+// pub type Line = [i8; 8];
 pub type Answer = usize;
 
 pub struct Input {
-    reports: Vec<Line>,
+    flat_reports: [i8; N * 8],
 }
 
 impl FromStr for Input {
-    type Err = ();
+    type Err = !;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed_lines = unsafe {
-            s.split_terminator('\n').map(|line| {
+        let mut i = 0;
+        let mut flat_reports = [0; N * 8];
+
+        unsafe {
+            s.lines().for_each(|line| {
                 let segments = line
                     .split_ascii_whitespace()
                     .map(|seg| seg.parse::<i8>().unwrap_unchecked());
-                segments.collect()
+
+                flat_reports[i..i + 8]
+                    .iter_mut()
+                    .zip(segments)
+                    .for_each(|(level, seg)| {
+                        *level = seg;
+                    });
+
+                i += 8;
             })
         };
 
-        let reports = parsed_lines.collect();
-
-        Ok(Self { reports })
+        Ok(Self { flat_reports })
     }
 }
 
@@ -32,39 +46,37 @@ impl FromStr for Input {
 pub fn part1(text: &str) -> Answer {
     let input: Input = unsafe { text.parse().unwrap_unchecked() };
 
-    let total = input.reports.len();
     let unsafe_reports = input
-        .reports
-        .iter()
-        .filter(|report| is_report_unsafe(&report));
-    total - unsafe_reports.count()
+        .flat_reports
+        .array_chunks()
+        .into_iter()
+        .filter(|report| is_report_unsafe(**report));
+
+    N - unsafe_reports.count()
 }
 
 #[aoc(day2, part2)]
 pub fn part2(text: &str) -> Answer {
     let input: Input = unsafe { text.parse().unwrap_unchecked() };
 
-    let total = input.reports.len();
     let inital_failed = input
-        .reports
+        .flat_reports
+        .array_chunks()
         .into_iter()
-        .filter(|report| is_report_unsafe(report));
-    let unsafe_reports = inital_failed.filter(|report| bruteforce_retest_is_report_unsafe(report));
+        .filter(|report| is_report_unsafe(**report));
 
-    total - unsafe_reports.count()
+    let unsafe_reports =
+        inital_failed.filter(|report| bruteforce_retest_is_report_unsafe(**report));
+
+    N - unsafe_reports.count()
 }
 
-fn bruteforce_retest_is_report_unsafe(report: &[i8]) -> bool {
-    for i in 0..report.len() {
-        let (start, rest) = report.split_at(i);
-        let end = match rest {
-            [_i, rest @ ..] => rest,
-            [rest @ ..] => rest,
-        };
+fn bruteforce_retest_is_report_unsafe(report: [i8; 8]) -> bool {
+    for i in 0..8 {
+        let mut cloned_report = report.clone();
+        cloned_report[i] = 0;
 
-        let test_slice = [start, end].concat();
-
-        if !is_report_unsafe(&test_slice) {
+        if !is_report_unsafe(cloned_report) {
             return false;
         }
     }
@@ -72,42 +84,61 @@ fn bruteforce_retest_is_report_unsafe(report: &[i8]) -> bool {
     true
 }
 
-fn is_report_unsafe(report: &[i8]) -> bool {
-    let diff = report[1] - report[0]; // positive for increasing, negative for decreasing
-    let mut steady = diff != 0; // edge-case where report[1] == report[0]
 
-    let mut max_adj_diff = BinaryHeap::with_capacity(report.len());
-    max_adj_diff.push(diff.abs());
+fn is_report_unsafe(report: [i8; 8]) -> bool {
+    let mut initial_dir = None;
+    let mut previous = None;
 
-    let mut previous = &report[1];
+    for level in report {
+        // skip any 0s in the parsed report
+        if level == 0 {
+            continue;
+        }
 
-    for level in &report[2..] {
-        let this_diff = *level - previous;
-        steady &= (diff * this_diff) > 0;
-        previous = level;
-        max_adj_diff.push(this_diff.abs());
+        // if this is the first non-zero level, set it as starting point
+        let Some(compare) = previous else {
+            previous = Some(level);
+            continue;
+        };
+
+        previous = Some(level);
+
+        let diff = level - compare;
+        if diff.abs() > 3 {
+            return true;
+        }
+
+        let dir = diff.signum();
+
+        // if this is the second non-zero level, set the inital direction
+        let Some(initial_dir) = initial_dir else {
+            initial_dir = Some(dir);
+            continue;
+        };
+
+        // if the new dir diverges from the previous dir, this report is unsafe, exit early
+        if initial_dir * dir <= 0 {
+            return true;
+        }
     }
 
-    let mad = max_adj_diff.pop().unwrap();
-
-    !steady || mad > 3
+    false // no early returns => this report is safe
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_DATA: &str = r#"7 6 4 2 1
+
+    const TEST_DATA: &str = r"7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9
-"#;
 
-    const EDGE_CASE_IGNORE_FIRST: &str = r#"45 44 46 47 50 52 54"#;
-    const EDGE_CASE_IGNORE_LAST: &str = r#"44 46 47 50 52 54 54"#;
-
+";
+  
     #[test]
     fn test_part1() {
         let result = part1(TEST_DATA);
@@ -118,17 +149,5 @@ mod tests {
     fn test_part2() {
         let result = part2(TEST_DATA);
         assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn test_part2_edge_case_ignore_first() {
-        let result = part2(EDGE_CASE_IGNORE_FIRST);
-        assert_eq!(result, 1);
-    }
-
-    #[test]
-    fn test_part2_edge_case_ignore_last() {
-        let result = part2(EDGE_CASE_IGNORE_LAST);
-        assert_eq!(result, 1);
     }
 }
