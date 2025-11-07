ab705bbdd8c2f6d7623cae90bcb7c2d39a316cb7
Maximillian Pfeil <pfeil.maximillian@gmail.com>
2025-11-07 22:05:49 +0100

    fix day 1 error

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,5 @@
 use ahash::AHashMap;
-use std::str::FromStr;
+use std::{collections::HashMap, str::FromStr};
 
 use aoc_runner_derive::aoc;
 
@@ -15,14 +15,14 @@ impl FromStr for Input {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed_lines = unsafe {
+        let parsed_lines = {
             s.split_terminator('\n').map(|line| {
                 let mut segments = line
-                    .split_ascii_whitespace()
-                    .map(|seg| seg.parse::<u32>().unwrap_unchecked());
+                    .split("   ")
+                    .map(|seg| seg.parse::<u32>().unwrap());
                 (
-                    segments.next().unwrap_unchecked(),
-                    segments.next().unwrap_unchecked(),
+                    segments.next().unwrap(),
+                    segments.next().unwrap(),
                 )
             })
         };
@@ -38,7 +38,7 @@ impl FromStr for Input {
 
 #[aoc(day1, part1)]
 pub fn part1(text: &str) -> Answer {
-    let mut input: Input = unsafe { text.parse().unwrap_unchecked() };
+    let mut input: Input = text.parse().unwrap() ;
 
     radsort::sort(&mut input.left_column);
     radsort::sort(&mut input.right_column);
@@ -52,12 +52,12 @@ pub fn part1(text: &str) -> Answer {
 
 #[aoc(day1, part2)]
 pub fn part2(text: &str) -> Answer {
-    let input: Input = unsafe { text.parse().unwrap_unchecked() };
+    let input: Input = text.parse().unwrap();
 
     let mut appearances = AHashMap::with_capacity(input.right_column.len());
 
     input.right_column.iter().for_each(|value| {
-        *appearances.entry(value).or_insert(0) += 1;
+        *appearances.entry(*value).or_insert(0) += 1;
     });
 
     input.left_column.iter().fold(0, |acc, left| {
